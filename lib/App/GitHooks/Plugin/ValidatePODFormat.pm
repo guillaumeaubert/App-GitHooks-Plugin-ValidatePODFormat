efc35334239a7ab350e09ee64fe0016808766e85
Guillaume Aubert <aubertg@cpan.org>
2014-06-08 15:40:09 -0700

    Release version 1.0.1.

--- lib/App/GitHooks/Plugin/ValidatePODFormat.pm
+++ lib/App/GitHooks/Plugin/ValidatePODFormat.pm
@@ -22,11 +22,11 @@ App::GitHooks::Plugin::ValidatePODFormat - Validate POD format in Perl and POD f
 
 =head1 VERSION
 
-Version 1.0.0
+Version 1.0.1
 
 =cut
 
-our $VERSION = '1.0.0';
+our $VERSION = '1.0.1';
 
 
 =head1 METHODS
